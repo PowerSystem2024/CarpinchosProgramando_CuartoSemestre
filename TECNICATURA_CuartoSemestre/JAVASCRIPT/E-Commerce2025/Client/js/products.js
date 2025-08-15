b584e5ceb31f8e7624fa45c80f91d446fd25e259
wandaoriana <wandalanatta5bod@gmail.com>
2025-08-14 21:00:15 -0300

    Agregando Clase01 de Javascript

---
+++ TECNICATURA_CuartoSemestre/JAVASCRIPT/E-Commerce2025/Client/js/products.js
@@ -0,0 +1,33 @@
+const productos = [
+  {
+    id: 1,
+    productName: "Coca-Cola",
+    price: 1400,
+    quanty: 1,
+    img: "/Users/mac/Desktop/CarpinchosProgramando_CuartoSemestre/TECNICATURA_CuartoSemestre/JAVASCRIPT/E-Commerce2025/Client/media/coca-cola.png",
+  },
+
+  {
+    id: 1,
+    productName: "Leche en polvo",
+    price: 800,
+    quanty: 1,
+    img: "/Users/mac/Desktop/CarpinchosProgramando_CuartoSemestre/TECNICATURA_CuartoSemestre/JAVASCRIPT/E-Commerce2025/Client/media/lechenpolvo.png",
+  },
+
+  {
+    id: 1,
+    productName: "Fideos instantáneos",
+    price: 1000,
+    quanty: 1,
+    img: "/Users/mac/Desktop/CarpinchosProgramando_CuartoSemestre/TECNICATURA_CuartoSemestre/JAVASCRIPT/E-Commerce2025/Client/media/fideos2.png",
+  },
+
+   {
+    id: 1,
+    productName: "Atún",
+    price: 1500,
+    quanty: 1,
+    img: "/Users/mac/Desktop/CarpinchosProgramando_CuartoSemestre/TECNICATURA_CuartoSemestre/JAVASCRIPT/E-Commerce2025/Client/media/atun.png",
+  },
+];
