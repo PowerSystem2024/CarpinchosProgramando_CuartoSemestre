f57c15aa1c5478c05964b64255d2945389af856c
Meratim <guerra.mercedes@hotmail.com>
2025-08-19 03:12:15 -0300

    Agrego clase 2 de JavaScript - E-Commerce

--- TECNICATURA_CuartoSemestre/JAVASCRIPT/E-Commerce2025/Client/js/index.js
+++ TECNICATURA_CuartoSemestre/JAVASCRIPT/E-Commerce2025/Client/js/index.js
@@ -1,4 +1,5 @@
 const shopContent = document.getElementById("shopContent");
+const cart = []; //El carrito es un array vacío
 
 productos.forEach((product) =>{
     const content = document.createElement("div");
@@ -8,4 +9,21 @@ productos.forEach((product) =>{
         <p>$ ${product.price} </p>
     `;
     shopContent.append(content);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    // Agregamos el botón al contenido del producto
+    content.appendChild(buyButton);
+
+    buyButton.addEventListener("click", () => { // Agregamos un evento click al botón para agregar el producto al carrito
+        cart.push({
+            id: product.id,
+            productName: product.productName,
+            price: product.price,
+            quantity: product.quantity,
+            img: product.img
+        })
+    console.log(cart);
+    })
 });
