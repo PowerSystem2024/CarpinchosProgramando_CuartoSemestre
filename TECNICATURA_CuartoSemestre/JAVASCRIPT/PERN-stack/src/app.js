0e46ee708fa89e2b0a3ea35c0e0790215f95f1df
Mariana Aguilera <marian5174@gmail.com>
2025-09-10 16:59:44 -0300

    Lanzamiento del servidor

--- TECNICATURA_CuartoSemestre/JAVASCRIPT/PERN-stack/src/app.js
+++ TECNICATURA_CuartoSemestre/JAVASCRIPT/PERN-stack/src/app.js
@@ -4,7 +4,18 @@ import morgan from "morgan";
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
 app.get("/", (req, res) => res.json({message: "Bienvenidos a mi proyecto"}));
+
+
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: "error",
+        message: err.message
+    });
+});
 
 export default app;
