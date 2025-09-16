2f6216ffd9b50ccdc9464130ef70897a390527e4
Mariana Aguilera <marian5174@gmail.com>
2025-09-15 23:13:55 -0300

    Clase 5, Pern

--- TECNICATURA_CuartoSemestre/Node/Clase03/app2.js
+++
@@ -1,6 +0,0 @@
-const saludar = (nombre) =>{ // Funcion de flecha
-    return `Saludos de ${nombre}`;
-}
-
-// esto es igual a: 'Saludos' + nombre;
-console.log(saludar('Ariel'));
--- TECNICATURA_CuartoSemestre/PYTHON/Clase01/laboratorio_usuarios/main.py
+++
@@ -1,4 +0,0 @@
-from app.menu_app_usuario import mostrar_menu
-
-if __name__ == '__main__':
-    mostrar_menu()
