0d5819bb4ce9e07ccfb0ef555e5db5a7b5b4a2f6
Nicolas E Mercado <mercadonicolas429@gmail.com>
2025-09-25 21:00:29 -0300

    Agrego la Clase06 del video 2 de la Clase06-A de node

--- TECNICATURA_CuartoSemestre/Node/Clase06/entorno.js
+++ TECNICATURA_CuartoSemestre/Node/Clase06/entorno.js
@@ -1,3 +1,6 @@
-let nombre = process.env.NOMBRE;
+let nombre = process.env.NOMBRE || "Sin nombre";
+let web = process.env.WEB || "No tengo web";
 
-console.log("Hola" + nombre);
+console.log("Hola " + nombre);
+console.log("Mi web es: " + web);
+
