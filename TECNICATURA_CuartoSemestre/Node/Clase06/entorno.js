70611113d8d072f313d6e278eb3c7592229620db
Nicolas E Mercado <mercadonicolas429@gmail.com>
2025-09-25 21:26:24 -0300

    Agrego la Clase06 del video 1 y 2 de la Clase 06-B

--- TECNICATURA_CuartoSemestre/Node/Clase06/entorno.js
+++ TECNICATURA_CuartoSemestre/Node/Clase06/entorno.js
@@ -4,3 +4,6 @@ let web = process.env.WEB || "No tengo web";
 console.log("Hola " + nombre);
 console.log("Mi web es: " + web);
 
+console.log("Ultima parte");
+console.log("Otra linea de codigo");
+console.log("Otra linea de codigo mas");
