5fc7dbab197bc0de149bcd8ea904b4570f2fd0eb
Melina Aguilar <aguilarmelinaelizabeth@gmail.com>
2025-10-01 17:47:16 -0300

    Agrego Clase08 de JavaScript

--- TECNICATURA_CuartoSemestre/JAVASCRIPT/PERN-stack/src/router/auth.routes.js
+++ TECNICATURA_CuartoSemestre/JAVASCRIPT/PERN-stack/src/router/auth.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { singin,singup, singout, profile } from "../controllers/auth.controller.js";
+import { isAuth } from "../middlewares/authmiddleware.js";
 
 const router = Router();
 
@@ -9,6 +10,6 @@ router.post("/singup", singup);
 
 router.post("/singout", singout);
 
-router.get("/profile", profile);
+router.get("/profile",isAuth, profile);
 
 export default router;
