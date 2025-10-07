2fd20fc71f822ff8fa42e719752eb9c13168ffdc
ezequiel <quirozarielezequiel@gmail.com>
2025-10-07 00:19:37 -0300

    Agrego Clase09 de Python

---
+++ TECNICATURA_CuartoSemestre/PYTHON/Clase09/Juego_Tecnicatura/constantes.py
@@ -0,0 +1,11 @@
+import os
+
+# Dimensiones de la pantalla
+SCREEN_WIDTH = 1366
+SCREEN_HEIGHT = 768
+
+# Colores
+COLOR_LASER = (0, 0, 255)  # Azul
+
+# Ruta a los assets
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets_1')
