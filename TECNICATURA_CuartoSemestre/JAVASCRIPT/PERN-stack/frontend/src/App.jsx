6adfa90ac2ebdaaa2928f142b75b2fbcf09a221d
ezequiel <quirozarielezequiel@gmail.com>
2025-10-08 22:53:54 -0300

    Agrego Clase09 de JavaScript

--- TECNICATURA_CuartoSemestre/JAVASCRIPT/PERN-stack/frontend/src/App.jsx
+++ TECNICATURA_CuartoSemestre/JAVASCRIPT/PERN-stack/frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path='/perfil' element={<ProfilePage />} />
         <Route path='/tareas' element={<TareasPage />} />
         <Route path='/tareas/crear' element={<TareaFormPage />} />
-        <Route path='/tareas/editar/:id' element={<TareasFormPage />} />
+        <Route path='/tareas/editar/:id' element={<TareaFormPage />} />
     </Routes>
   )
 }
