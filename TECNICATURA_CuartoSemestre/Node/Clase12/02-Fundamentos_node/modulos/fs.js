f5a0a146b329f3f48c6da6fa7756957d991bea8f
Mariana Aguilera <marian5174@gmail.com>
2025-11-03 21:39:06 -0300

    Escribir y crear un archivo

--- TECNICATURA_CuartoSemestre/Node/Clase12/02-Fundamentos_node/modulos/fs.js
+++ TECNICATURA_CuartoSemestre/Node/Clase12/02-Fundamentos_node/modulos/fs.js
@@ -7,4 +7,19 @@ function leer(ruta, cb) {
     })
 }
 
-leer(`${__dirname}/archivo.txt`, console.log); //sintaxis ES6
+
+// Segundo: escribimos el archivo1.txt creandolo
+function escribir(ruta, contenido, cb){
+    fs.writeFile(ruta, contenido, function(err){
+     if (err){
+        console.log('Nose ha podido escribir', err);
+     }else{
+        console.log('se ha escrito correctamente');
+     }
+    })
+}
+
+escribir(`${__dirname}/archivo1.txt`,'Reescribimos el archivo', console.log);
+//leer(`${__dirname}/archivo.txt`, console.log); //sintaxis ES6
+
+        
